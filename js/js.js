724474401c90265901de1c6d84ee883e75ac50c5
kimmabrandt <kimmabrandt@gmail.com>
2016-12-20 15:33:35 -0800

    contact, animated header

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$(function(){
+  $("#typed").typed({
+    strings:["Web Developer","Designer","Photographer","Cat Lover"],
+    typeSpeed: 90,
+    backDelay: 3000,
+    loop: true,
+    callback: function(){}
+  });
+});
